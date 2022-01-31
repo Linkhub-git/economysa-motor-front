f695605e80daab5d07c2f179907eff04b2765e9b
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-31 19:08:31 -0300

    Added the btn

--- src/components/Motor/Motor.js
+++ src/components/Motor/Motor.js
@@ -64,6 +64,11 @@ function Motor() {
                                         <Form.Group className="mb-3 inputUsuario" controlId="formBasicEmail">
                                           <Form.Control type="text" placeholder="Código" />
                                         </Form.Group>
+
+                                        <Form.Group className="mb-3 inputUsuario" controlId="formBasicPassword">
+                                          <Form.Control type="email" placeholder="Correo electrónico" />
+                                        </Form.Group>
+                                        <button className='btn-search'>Buscar</button>
                                       </Form>
                                   </div>
                                   <p className='textRegistrosEncontrados'>registro(s) encontrados</p>

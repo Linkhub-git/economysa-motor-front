96591d22f39996f1fde752cd8d327178f57e8942
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-01 17:25:11 -0300

    Almost done with the sidebar responsive, i' got to resolve a problem in the header

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -3,7 +3,7 @@ import './Header.css';
 import Logo from '../../images/logo.png'
 
 function HeaderMobile() {
-  const [showSideBar1, setShowSideBar1] = useState(false);
+  const [showSideBar1, setShowSideBar1] = useState(true);
   const [showSideBar2, setShowSideBar2] = useState(false);
   const [showSideBar3, setShowSideBar3] = useState(false);
   /* Utilizar acá el showSideBar3, que es el que muestra el span en versión mobile 
@@ -21,7 +21,7 @@ function HeaderMobile() {
                   <p>Mirage</p>
               </div>
               <div className='headerBtnContainerMobile'>
-                <a href="#" onClick={()=> setShowSideBar1(!showSideBar1)} className={showSideBar1 === false ? "firstLinkHeader" : "firstLinkHeader activeHeader"}> <i className="fas fa-bars"></i></a>
+                <a href="#" onClick={()=> setShowSideBar1(!showSideBar1)} className={showSideBar1 === true ? "firstLinkHeader" : "firstLinkHeader activeHeader"}> <i className="fas fa-bars"></i></a>
                 <a href="#" onClick={()=> setShowSideBar2(!showSideBar2)} className={showSideBar2 === false ? "middleLinkHeader" : "middleLinkHeader activeHeader"}><i className="fas fa-align-right"></i></a>
                 <a href="#" onClick={()=> setShowSideBar3(!showSideBar3)} className={showSideBar3 === false ? "lastLinkHeader" : "lastLinkHeader activeHeader activeLasLink"}><i className="fas fa-ellipsis-v"></i></a>
               </div>

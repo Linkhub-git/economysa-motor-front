b90835671d4ad0dc854a3a84a5a0f47fc21ba525
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-01 15:22:11 -0300

    Done with the logic, now i got to finish the hover styles

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -17,7 +17,7 @@ function HeaderMobile() {
             <div className='headerBtnContainerMobile'>
               <a href="#" onClick={()=> setShowSideBar1(!showSideBar1)} className={showSideBar1 === false ? "firstLinkHeader" : "firstLinkHeader activeHeader"}> <i className="fas fa-bars"></i></a>
               <a href="#" onClick={()=> setShowSideBar2(!showSideBar2)} className={showSideBar2 === false ? "middleLinkHeader" : "middleLinkHeader activeHeader"}><i className="fas fa-align-right"></i></a>
-              <a href="#" onClick={()=> setShowSideBar3(!showSideBar3)} className={showSideBar3 === false ? "lastLinkHeader" : "lastLinkHeader activeHeader"}><i className="fas fa-ellipsis-v"></i></a>
+              <a href="#" onClick={()=> setShowSideBar3(!showSideBar3)} className={showSideBar3 === false ? "lastLinkHeader" : "lastLinkHeader activeHeader activeLasLink"}><i className="fas fa-ellipsis-v"></i></a>
             </div>
       </div>
     </header>

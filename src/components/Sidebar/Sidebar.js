23175174745a7ba671666f83138edbf90a6a6efb
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-01 20:25:51 -0300

    Now the arrow in the sidebar change the color when the user click a link

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -26,56 +26,59 @@ function Sidebar(props) {
 
   Luego, por otra parte, lo que tengo que hacer es jugar con los eventos onClick para dejar de manera
   fija el sidebar, eso se puede hacer añadiendo clases para alterar el position absolute
-  Simplemente es cuestión de añadir o remover el position absolute, luego ver el evento onMouse o hover */
+  Simplemente es cuestión de añadir o remover el position absolute, luego ver el evento onMouse o hover 
+  
+  Para pasar la información de si el sidebar está oculto o no, al padre, definir un estado y una
+  función DESDE EL PADRE, y recibir el dato actualizado del hijo, para trabajar en base a ello*/
   console.log(hideSidebarDesk);
-  return (
-    <aside ref={sideBar} className={props.btnShowSideBar === false ? "sidebarContainer" : "sidebarContainer activeShow"}>
-    <div className='iconTitleSideBar'>
-        <Link className='linkHeaderSidebar' to="/">
-            <img src={Logo} alt="ej" />
-            <span>Economysa</span>
-        </Link>
-
-      <button className={hideSidebarDesk === true ? "" : "sideBarHideDesktop"} onClick={() => setHideSidebarDesk(!hideSidebarDesk)}><i className={hideSidebarDesk === true ? "fas fa-lock" : "fas fa-lock-open"}></i></button>
-      
-    </div>
-    <div className='linksContainer'>
-        <Link className='linkSidebar' to="/">Dasboard <i className="fas fa-home"></i></Link>
-        <Accordion>
-            <Accordion.Item eventKey="0" className='contentContainerLink'>
-                <Accordion.Header className='titleLink'>Seguridad <i className="fas fa-lock lockLink"></i></Accordion.Header>
-                  <Accordion.Body className='contentInsideLink'>
-                      <div className="individualLinkContainer">
-                        <Link to="/Seguridad" className='linkBar'>Usuario <i className="fas fa-users"></i></Link>
-                      </div>
-                  </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1" className='contentContainerLink'>
-                <Accordion.Header  className='titleLink'>Motor <i className="fas fa-globe-americas"></i></Accordion.Header>
-                  <Accordion.Body className='contentInsideLink'>
-                      <div className="individualLinkContainer">
-                        <Link to="/motor/1" className='linkBar'>Clientes<i className="fas fa-list-ul"></i></Link>
-                      </div>
-                      <div className="individualLinkContainer">
-                        <Link to="/motor/2" className='linkBar'>Direcciones Entrega <i className="fas fa-folder"></i></Link>
-                      </div>
-                      <div className="individualLinkContainer">
-                        <Link to="/" className='linkBar'>Productos <i className="fas fa-bookmark"></i></Link>
-                      </div>
-                      <div className="individualLinkContainer">
-                        <Link to="/" className='linkBar'>Mecánicas <i className="fas fa-bookmark"></i></Link>
-                      </div>
-                      <div className="individualLinkContainer">
-                        <Link to="/" className='linkBar'>Proveedores <i className="fas fa-bookmark"></i></Link>
-                        
-                      </div>
-                      <div className="individualLinkContainer">
-                        <Link to="/" className='linkBar'>Vendedores <i className="fas fa-bookmark"></i></Link>
-                      </div>
-                  </Accordion.Body>
-            </Accordion.Item>
-        </Accordion>
-    </div>
+  return(
+    <aside className={hideSidebarDesk === true ? "asddsa" : "sideBarHideDesktop"}>
+          <div ref={sideBar} className={props.btnShowSideBar === false ? "sidebarContainer" : "sidebarContainer activeShow"}>
+            <div className='iconTitleSideBar'>
+                <Link className='linkHeaderSidebar' to="/">
+                    <img src={Logo} alt="ej" />
+                    <span>Economysa</span>
+                </Link>
+              <button onClick={() => setHideSidebarDesk(!hideSidebarDesk)}><i className={hideSidebarDesk === true ? "fas fa-lock" : "fas fa-lock-open"}></i></button>
+            </div>
+            <div className='linksContainer'>
+                <Link className='linkSidebar' to="/">Dasboard <i className="fas fa-home"></i></Link>
+                    <Accordion>
+                        <Accordion.Item eventKey="0" className='contentContainerLink'>
+                            <Accordion.Header className='titleLink'>Seguridad <i className="fas fa-lock lockLink"></i></Accordion.Header>
+                                <Accordion.Body className='contentInsideLink'>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/Seguridad" className='linkBar'>Usuario <i className="fas fa-users"></i></Link>
+                                    </div>
+                                </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1" className='contentContainerLink'>
+                            <Accordion.Header  className='titleLink'>Motor <i className="fas fa-globe-americas"></i></Accordion.Header>
+                                <Accordion.Body className='contentInsideLink'>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/motor/1" className='linkBar'>Clientes<i className="fas fa-list-ul"></i></Link>
+                                    </div>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/motor/2" className='linkBar'>Direcciones Entrega <i className="fas fa-folder"></i></Link>
+                                    </div>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/" className='linkBar'>Productos <i className="fas fa-bookmark"></i></Link>
+                                    </div>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/" className='linkBar'>Mecánicas <i className="fas fa-bookmark"></i></Link>
+                                    </div>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/" className='linkBar'>Proveedores <i className="fas fa-bookmark"></i></Link>
+                                      
+                                    </div>
+                                    <div className="individualLinkContainer">
+                                      <Link to="/" className='linkBar'>Vendedores <i className="fas fa-bookmark"></i></Link>
+                                    </div>
+                                </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+            </div>
+        </div>
     </aside>
   );
 }

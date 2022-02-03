727354e227cc5a9a15a75be9dd7dbc6f590b744d
Franco Ezequiel <eze.cavs@hotmail.com>
2022-02-03 18:22:49 -0300

    Solved a problem to change the styles in the accordion in the header mobile

--- src/components/Header/HeaderMobile.js
+++ src/components/Header/HeaderMobile.js
@@ -48,26 +48,28 @@ function HeaderMobile() {
                         <span className='puestOHeaderHideSpanMobile'>Design</span>
                     </div>
                 </div>
-                <Accordion defaultActiveKey="0">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>Accordion Item #1</Accordion.Header>
-                          <Accordion.Body>
-                            Lorem ipsum 
-                          </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>Accordion Item #2</Accordion.Header>
-                          <Accordion.Body>
-                            Lorem ipsum 
-                          </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="2">
-                        <Accordion.Header>Accordion Item #2</Accordion.Header>
-                          <Accordion.Body>
-                            Lorem ipsum 
-                          </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+                <div className='prueba'>
+                    <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Accordion Item #1</Accordion.Header>
+                              <Accordion.Body>
+                                Lorem ipsum 
+                              </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Accordion Item #2</Accordion.Header>
+                              <Accordion.Body>
+                                Lorem ipsum 
+                              </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Accordion Item #2</Accordion.Header>
+                              <Accordion.Body>
+                                Lorem ipsum 
+                              </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
             </div>
         </div>
       </header>
